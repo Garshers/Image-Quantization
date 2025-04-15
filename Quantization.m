8be2bd4d2e6a9017b26c834af8711702a519267b
Krzysztof <krzysztof.pawik@gmail.com>
2025-04-15 16:31:46 +0200

    Changing counting unique color method (not to use sort). Correcting hsv quantization

--- Quantization.m
+++ Quantization.m
@@ -6,44 +6,92 @@ function [elapsedTime, uniqueCount] = countUniqueColors(filename, pathname, mode
 
     % Check if the image is RGB
     if size(imageRGB, 3) ~= 3
-    error('Image must be an RGB image.');
+        error('Image must be an RGB image.');
     end
-    
-    % Checking mode
+
+    %checking mode, conuting colors
     if strcmp(mode, 'HSV')
         image = rgb2hsv(imageRGB);
+        tic;% -------------------------------Start timer--------------
+        uniqueCount = uniqueColorCounterHSV(image);
+        elapsedTime = toc * 1000; % ----------Stop timer-----------
+        imageToSave = hsv2rgb(image); % Convert back to RGB
+        
     elseif strcmp(mode, 'RGB')
         image = imageRGB;
-    else
-        error('Invalid mode. Use "RGB" or "HSV".');
-    end
-    
-    tic;% -------------------------------Start timer--------------
-    uniqueCount = uniqueColorCounter(image);
-    elapsedTime = toc * 1000; % ----------Stop timer-----------
-    
-    % Saving images
-    if strcmp(mode, 'HSV')
-        imageToSave = hsv2rgb(image); % Convert back to RGB
-    elseif strcmp(mode, 'RGB')
+        tic;% -------------------------------Start timer--------------
+        uniqueCount = uniqueColorCounterRGB(image);
+        elapsedTime = toc * 1000; % ----------Stop timer-----------
         imageToSave = imageRGB; % Keep RGB
     else
         error('Invalid mode. Use "RGB" or "HSV".');
     end
+
     prefix = ['_', mode];
     [pathstr, name, ext] = fileparts(fullFilePath);
     saveFilename = fullfile(pathstr, [name, prefix, ext]);
     imwrite(imageToSave, saveFilename);
 end
 
-function uniqueCount = uniqueColorCounter(image)
+function uniqueCount = uniqueColorCounterRGB(image)
+    % Reshape the image into a list of pixels (each row is [R G B])
     pixels = reshape(image, [], 3);
-    pixelsSorted = sortrows(pixels); % Sort pixels red -> green -> blue
-    numPixels = size(pixelsSorted, 1);
-    uniqueCount = 1;
-    for i = 2:numPixels
-        if ~isequal(pixelsSorted(i, :), pixelsSorted(i-1, :)) % If prev is not eq curr
-          uniqueCount = uniqueCount + 1;
+    numPixels = size(pixels, 1);
+
+    maxColorValue = 256;
+    colorSpaceSize = (maxColorValue)^3;% Total size of the RGB color space
+    seenColors = false(colorSpaceSize, 1); % Create a boolean array to keep track of seen colors
+    
+    uniqueCount = 0; % Unique color count
+    
+    % Iterate through each pixel
+    for i = 1:numPixels
+        r = pixels(i, 1);
+        g = pixels(i, 2);
+        b = pixels(i, 3);
+        
+        % Calculate a unique index for the RGB color
+        colorIndex = uint32(r) * maxColorValue^2 + uint32(g) * maxColorValue + uint32(b) + 1;
+        
+        % If color has not been seen before
+        if ~seenColors(colorIndex)
+            % Mark this color as seen
+            seenColors(colorIndex) = true;
+            uniqueCount = uniqueCount + 1;
+        end
+    end
+end
+
+function uniqueCount = uniqueColorCounterHSV(image)
+    % Reshape the image into a list of pixels (each row is [H S V])
+    pixels = reshape(image, [], 3);
+    numPixels = size(pixels, 1);
+    
+    % Create a map to store seen HSV colors (key: string representation, value: logical true)
+    seenColorsHSV = containers.Map('KeyType', 'char', ...
+                                    'ValueType', 'logical');
+
+    uniqueCount = 0; % Unique color count
+    
+    % Iterate through each pixel in the HSV image
+    for i = 1:numPixels
+        H = pixels(i, 1);
+        S = pixels(i, 2);
+        V = pixels(i, 3);
+        
+        % Round the HSV values to a .6 precision and convert them to strings
+        hueKey = sprintf('%.6f', H);
+        saturationKey = sprintf('%.6f', S);
+        valueKey = sprintf('%.6f', V);
+        
+        % Create a unique key string by concatenating the rounded HSV values
+        colorKey = [hueKey, '-', saturationKey, '-', valueKey];
+        
+        % If color has not been seen before
+        if ~isKey(seenColorsHSV, colorKey)
+            % Mark this color as seen
+            seenColorsHSV(colorKey) = true;
+            uniqueCount = uniqueCount + 1;
         end
     end
 end
@@ -51,8 +99,8 @@ end
 function test1()
     timeSumRGB = 0;
     timeSumHSV = 0;
-    n = 20;
-    [filename, pathname] = uigetfile({'*.jpg;*.png', 'Image Files (*.jpg, *.png)'}, 'Select an RGB Image');
+    n = 1;
+    [filename, pathname] = uigetfile({'*.jpg;*.png;*.bmp', 'Image Files (*.jpg, *.png)'}, 'Select an RGB Image');
     if isequal(filename, 0)
         disp('User canceled file selection.');
         return;
@@ -101,17 +149,16 @@ function quantizeImage(filename, pathname, divisions, mode)
         quantizedHSV = quantizeHSV(imageHSV, divisions);
         quantizedImage = hsv2rgb(quantizedHSV);
         quantizedImage = uint8(round(quantizedImage * 255));
+        numUniqueColors = uniqueColorCounterHSV(quantizedImage);
     elseif strcmp(mode, 'RGB')
         quantizedImage = quantizeRGB(image, divisions);
+        numUniqueColors = uniqueColorCounterRGB(quantizedImage);
     else
         error('Invalid mode. Use "RGB" or "HSV".');
     end    
 
     % Calculate PSNR
     psnrValue = psnr(quantizedImage, image);
-
-    % Calculate unique colors
-    numUniqueColors = uniqueColorCounter(quantizedImage);
 
     % Display results
     fprintf('\nDivisions: %dx%dx%d\n', divisions(1), divisions(2), divisions(3));
@@ -141,23 +188,31 @@ function quantizedImage = quantizeRGB(imageRGB, divisions)
 end
 
 function quantizedHSV = quantizeHSV(imageHSV, divisions)
-    % Quantize HSV image according to specified divisions
     quantizedHSV = imageHSV;
+    height = size(imageHSV, 1);
+    width = size(imageHSV, 2);
 
-    % Quantize H, S, V channels directly using a loop
-    for channel = 1:3
-        levels = linspace(0, 1, divisions(channel));
-        shiftedImage = imageHSV(:, :, channel) - (levels(2) - levels(1)) / 2;
-        quantizedHSV(:, :, channel) = levels(imquantize(shiftedImage, levels));
-    end
+    for i = 1:height
+        for j = 1:width
+            % Quantize Hue (cyclic)
+            hueLevels = linspace(0, 1, divisions(1) + 1); % divisions + 1 levels to create 'divisions' bins
+            [~, h_idx] = min(abs(imageHSV(i, j, 1) - hueLevels(1:end-1)));
+            quantizedHSV(i, j, 1) = hueLevels(h_idx);
 
-    % Handle achromatic colors
-    achromaticIndices = find(imageHSV(:, :, 2) == 0);
-    if ~isempty(achromaticIndices)
-        levelsAchromatic = linspace(0, 1, 6); % 6 achromatic levels
-        validIndices = achromaticIndices(achromaticIndices <= size(imageHSV, 1));
-        if ~isempty(validIndices)
-            quantizedHSV(validIndices, 3) = levelsAchromatic(imquantize(imageHSV(validIndices, 3), levelsAchromatic));
+            % Quantize Saturation
+            satLevels = linspace(0, 1, divisions(2));
+            [~, s_idx] = min(abs(imageHSV(i, j, 2) - satLevels));
+            quantizedHSV(i, j, 2) = satLevels(s_idx);
+
+            % Quantize Value
+            valLevels = linspace(0, 1, divisions(3));
+            [~, v_idx] = min(abs(imageHSV(i, j, 3) - valLevels));
+            quantizedHSV(i, j, 3) = valLevels(v_idx);
+
+            % Handle achromatic colors: force Saturation to 0
+            if imageHSV(i, j, 2) == 0
+                quantizedHSV(i, j, 2) = 0;
+            end
         end
     end
 end
@@ -170,16 +225,16 @@ function testQuantization()
         return;
     end
 
-    divisionsList = {[2, 2, 2], [4, 4, 4], [4, 6, 4], [8, 8, 4]};
+    divisionsList = {[2, 2, 2], [4, 4, 4], [4, 8, 4], [8, 8, 4]};
     for i = 1:length(divisionsList)
         quantizeImage(filename, pathname, divisionsList{i}, 'RGB');
     end
 
-    divisionsList = {[2, 2, 2], [4, 4, 4], [4, 6, 4], [10, 5, 5]};
+    divisionsList = {[2, 2, 2], [4, 4, 4], [4, 8, 4], [10, 5, 5]};
     for i = 1:length(divisionsList)
         quantizeImage(filename, pathname, divisionsList{i}, 'HSV');
     end
 end
 
-%test1();
-testQuantization();
+test1();
+%testQuantization();
